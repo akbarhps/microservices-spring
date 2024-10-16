239ccdd525e3f9eaf42a056ed3314d105419c8bf
ahps <akbarputrasrg@unimed.ac.id>
2024-10-16 11:47:02 +0700

    fix: authentication service

--- authentication-server/src/main/kotlin/com/charuniverse/microservices/authentication_server/controller/AuthenticationController.kt
+++ authentication-server/src/main/kotlin/com/charuniverse/microservices/authentication_server/controller/AuthenticationController.kt
@@ -7,26 +7,38 @@ import com.charuniverse.microservices.authentication_server.model.RegisterReques
 import com.charuniverse.microservices.authentication_server.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 
-@Controller
+@RestController
+@RequestMapping("/api/auth")
 class AuthenticationController(@Autowired val userRepository: UserRepository) {
 
-    @PostMapping("/api/register")
+    @GetMapping("/{token}")
+    fun getUserInfo(@PathVariable(name = "token") token: String): ResponseEntity<User> {
+        val user = userRepository.findById(token.substringAfter("token-"))
+        return if (user.isPresent) {
+            ResponseEntity.ok(user.get())
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/register")
     fun register(@RequestBody registerRequest: RegisterRequest): ResponseEntity<LoginResponse> {
-        val entity = User(username = registerRequest.username, password =  registerRequest.password)
+        val entity = User(username = registerRequest.username, password = registerRequest.password)
         val user = userRepository.save(entity)
         return ResponseEntity.ok(LoginResponse(user.username, "token-${user.id}"))
     }
 
-    @PostMapping("/api/login")
+    @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse> {
-        val user = userRepository.findFirstByUsername(loginRequest.username) ?: return ResponseEntity.badRequest().build()
+        val user = userRepository.findFirstByUsername(loginRequest.username)
+            ?: return ResponseEntity.badRequest().build()
+
         if (user.password != loginRequest.password) {
             return ResponseEntity.badRequest().build()
         }
+
         return ResponseEntity.ok(LoginResponse(user.username, "token-${user.id}"))
     }
 }
--- authentication-server/src/main/kotlin/com/charuniverse/microservices/authentication_server/repository/UserRepository.kt
+++ authentication-server/src/main/kotlin/com/charuniverse/microservices/authentication_server/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.charuniverse.microservices.authentication_server.repository
 import com.charuniverse.microservices.authentication_server.entity.User
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository : CrudRepository<User, Long> {
+interface UserRepository : CrudRepository<User, String> {
 
     fun findFirstByUsername(username: String): User?
 
